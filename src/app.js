8f013dd37ed7e8a5e30e2f90f496606a560d64f6
avexyk <avexyk@gmail.com>
2022-01-17 15:10:31 -0600

    Segunda versión de la página web del GitHub

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+const menuButton = document.getElementById('menu')
+const navList = document.getElementById('nav-list')
+
+function toggleButton() {
+  navList.classList.toggle('show')
+}
+
+menuButton.addEventListener('click', toggleButton)
